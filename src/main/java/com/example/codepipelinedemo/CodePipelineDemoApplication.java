58b3ad8a176142033f6e9c77ac61a678221f3e99
Kayode Sakeye <kayode.sakeye@mobpro.com>
2022-06-21 23:33:29 +0100

    add files

---
+++ src/main/java/com/example/codepipelinedemo/CodePipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.codepipelinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodePipelineDemoApplication.class, args);
+	}
+
+}
